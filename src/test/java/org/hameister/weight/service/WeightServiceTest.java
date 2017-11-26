1b6bd7e044f32663908b4e2ff77ba77999e0ba75
hameister <joern@hameister.org>
2017-11-26 10:05:16 +0100

    Added Importer

--- src/test/java/org/hameister/weight/service/WeightServiceTest.java
+++ src/test/java/org/hameister/weight/service/WeightServiceTest.java
@@ -1,28 +1,37 @@
 package org.hameister.weight.service;
 
 import org.hameister.weight.WeightReader;
+import org.hameister.weight.data.WeightData;
 import org.hameister.weight.data.WeightDataRepository;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by hameister on 28.10.17.
  */
-
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 public class WeightServiceTest {
 
     @Test
-    public void  testdataShouldContain3Datasets() throws IOException {
+    public void testdataShouldContain3Datasets() throws IOException {
         WeightDataRepository weightDataRepository = mock(WeightDataRepository.class);
+        when(weightDataRepository.findAll()).thenReturn(getTestdata());
 
-
-        WeightService weightService = new WeightService(new WeightReader(Paths.get("./src/test/resources/Testdata.txt")), weightDataRepository);
+        WeightService weightService = new WeightService(weightDataRepository);
 
         assertThat(weightService.numberOfLines(), is(3));
     }
@@ -30,9 +39,15 @@ public class WeightServiceTest {
     @Test
     public void avgShouldComputeTheAvagerateWeight() throws IOException {
         WeightDataRepository weightDataRepository = mock(WeightDataRepository.class);
+        when(weightDataRepository.findAll()).thenReturn(getTestdata());
 
-
-        WeightService weightService = new WeightService(new WeightReader(Paths.get("./src/test/resources/Testdata.txt")), weightDataRepository);
+        WeightService weightService = new WeightService(weightDataRepository);
         assertThat(weightService.getAvg(), is(80.36666870117188d));
+    }
+
+    public List<WeightData> getTestdata() {
+        return Arrays.asList(new WeightData(80.3f, LocalDate.of(2017, 8, 23)),
+                new WeightData(80.5f, LocalDate.of(2017, 8, 24)),
+                new WeightData(80.3f, LocalDate.of(2017, 8, 25)));
     }
 }
