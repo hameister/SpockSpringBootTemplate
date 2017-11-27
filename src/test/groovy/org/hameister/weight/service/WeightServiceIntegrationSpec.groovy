3d91a523dc5afc8b2e3f47a518907f92b8c8c058
hameister <joern@hameister.org>
2017-11-27 17:51:51 +0100

    Multi-Variable Data Pipes

---
+++ src/test/groovy/org/hameister/weight/service/WeightServiceIntegrationSpec.groovy
@@ -0,0 +1,93 @@
+package org.hameister.weight.service
+
+import org.hameister.weight.data.WeightData
+import org.hameister.weight.data.WeightDataRepository
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+
+/**
+ * Created by hameister on 27.11.17.
+ */
+class WeightServiceIntegrationSpec extends Specification {
+
+    def "Get weight at one date with parameterized tests"() {
+        given: "Repository with Testdata"
+        WeightDataRepository dataRepository = Mock(WeightDataRepository)
+        dataRepository.findByDate(date) >> new WeightData(weight, date)
+        WeightService weightService = new WeightService(dataRepository);
+
+
+        when: "WeightService is created and lines are read"
+        float weightFromDate = weightService.getWeightFromDate(date)
+
+        then: "The weight at this day"
+        weightFromDate == weight
+        where: "Load data"
+        weight | date
+        80.3f  | LocalDate.of(2017, 8, 23)
+        80.5f  | LocalDate.of(2017, 8, 24)
+        80.3f  | LocalDate.of(2017, 8, 25)
+
+    }
+
+
+    def "Get weight at one date with parameters from encapsulated method getTestData"() {
+        given: "Repository with Testdata"
+        WeightDataRepository dataRepository = Mock(WeightDataRepository)
+        dataRepository.findByDate(date) >> new WeightData(weight, date)
+        WeightService weightService = new WeightService(dataRepository);
+
+
+        when: "WeightService is created and lines are read"
+        float weightFromDate = weightService.getWeightFromDate(date)
+
+        then: "The weight at this day"
+        weightFromDate == weight
+        where: "Load data"
+        // ArrayList [Float, LocalDate]
+        [weight, date] << getTestdata()
+    }
+
+    //Create Multi-Variable Data Pipes for Groovy Spock test in a method.
+    def getTestdata() {
+        return [[80.3f, LocalDate.of(2017, 8, 23)], [80.5f, LocalDate.of(2017, 8, 24)]]
+    }
+
+
+    def "Get weight at one date with parameters from file"() {
+        given: "Repository with Testdata"
+        WeightDataRepository dataRepository = Mock(WeightDataRepository)
+        dataRepository.findByDate(date) >> new WeightData(weight, date)
+        WeightService weightService = new WeightService(dataRepository);
+
+
+        when: "WeightService is created and lines are read"
+        float weightFromDate = weightService.getWeightFromDate(date)
+
+        then: "The weight at this day"
+        weightFromDate == weight
+        where: "Load data"
+        // ArrayList [float, LocalDate]
+        [weight, date] << getTestdataFromFile()
+    }
+
+    //Create Multi-Variable Data Pipes for Groovy Spock test from a file.
+    def getTestdataFromFile() {
+
+        String testdata = new File("src/test/resources/Testdata.txt").text
+        ArrayList data = [];
+
+        List<String> lines = testdata.tokenize("\n")
+
+        for (String line : lines) {
+            float weight = Float.parseFloat(line.substring(0, 4).replace(',','.'));
+            def date = line.substring(5) + "2017";
+            data.add([weight, LocalDate.parse(date,DateTimeFormatter.ofPattern('dd.M.yyyy'))])
+        }
+        return data
+    }
+}
